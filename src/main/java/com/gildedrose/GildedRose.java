f26e49917d5f23e8cf715ae00576546a7d47fb6c
Joheartless <165768613+Joheartless@users.noreply.github.com>
2024-05-15 11:39:25 +0200

    Test

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -29,7 +29,7 @@ public class GildedRose {
 	 */
 	public void updateQuality() {
 		//hghghhghgh
-		for (int productIndex = 0; productIndex < items.length; productIndex++) {
+		fore (int productIndex = 0; productIndex < items.length; productIndex++) {
 			if (esSulfuras(productIndex))
 				continue; // con sulfuras no se hace nada
 			quitaUnDia(productIndex);
